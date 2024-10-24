0085beb615378af00a419bc80597d9f3f68b9e37
Lee Morgan <lee@leemorgan.dev>
2024-10-24 17:10:14 -0400

    Check token and data when vendor opens dashboard.

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,15 +1,48 @@
 <script>
     import "../../global.css";
+    import {onMount} from "svelte";
     import Notifier from "../../components/Notifier.svelte";
     import Loader from "../../components/Loader.svelte";
     import Menu from "./components/Menu.svelte";
 
     let notifier = $state({type: "", message: ""});
     let loader = $state(false);
+    let vendor;
 
     const changePage = (event)=>{
         console.log(event.detail.page);
     }
+
+    onMount(()=>{
+        const token = localStorage.getItem("vendorToken");
+        if(!token) window.location.href = "/login";
+
+        loader = true;
+        fetch(`${import.meta.env.VITE_API_URL}/vendor/self`, {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(r=>r.json())
+            .then((response)=>{
+                if(response.error){
+                    localStorage.removeItem("vendorToken");
+                    window.location.href = "/login";
+                }else{
+                    vendor = response;
+                }
+            })
+            .catch((err)=>{
+                console.log(err);
+                notifier.type = "error";
+                notifier.message = "Something went wrong, try refreshing the page";
+            })
+            .finally(()=>{
+                loader = false;
+            });
+    });
 </script>
 
 <Notifier type={notifier.type} message={notifier.message}/>
