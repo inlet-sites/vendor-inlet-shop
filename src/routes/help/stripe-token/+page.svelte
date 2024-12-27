bae9dda09e255746e8e6f990dbb1c32ecefb9e06
Lee Morgan <lee@leemorgan.dev>
2024-12-27 14:47:42 -0500

    Create basic page for displaying stripe token help.

---
+++ src/routes/help/stripe-token/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import "../../../global.css";
+</script>
+
+<div class="container">
+    <h1>How to get your Stripe API Token</h1>
+</div>
+
+<style>
+    .container{
+        color: white;
+    }
+</style>
