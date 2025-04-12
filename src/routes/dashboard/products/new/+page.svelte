d4a5c4c3e4bd41faa84f1f03f24d40ca69f74552
Lee Morgan <lee@leemorgan.dev>
2025-04-12 15:36:00 -0400

    Create base file for new products.

---
+++ src/routes/dashboard/products/new/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import ProductData from "./ProductData.svelte";
+
+    let stage = $state("productData");
+    let newProduct = $state();
+
+    const nextStage = (nextStage)=>{
+        stage = nextStage;
+    }
+</script>
+
+<svelte:head>
+    <title>Create Product | Vendor.Inlet.Shop</title>
+</svelte:head>
+
+<div class="NewProduct">
+    {#if stage === "productData"}
+        <ProductData
+            nextStage={nextStage}
+        />
+    {/if}
+</div>
