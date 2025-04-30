adf2c8cf84de6b77ff7808c45235b9334e3a0fa1
Lee Morgan <lee@leemorgan.dev>
2025-04-30 16:39:22 -0400

    Create funtionality for changing password.

--- src/routes/password/[vendorId]/[vendorToken]/+page.svelte
+++ src/routes/password/[vendorId]/[vendorToken]/+page.svelte
@@ -1,19 +1,72 @@
 <script>
     import "$lib/global.css";
+    import {goto} from "$app/navigation";
     import logo from "$lib/logo.png";
+    import Loader from "$lib/Loader.svelte";
+    import Notifier from "$lib/Notifier.svelte";
 
     let {data} = $props();
+    let loader = $state(false);
+    let notifier = $state(null);
     let password = $state();
     let confirmPassword = $state();
 
+    const notify = (type, message)=>{
+        notifier = {
+            type: type,
+            message: message
+        };
+
+        setTimeout(()=>{
+            notifier = null;
+        }, 7500);
+    }
+
     const submit = ()=>{
-        console.log("submit");
+        loader = true;
+        fetch(`${import.meta.env.VITE_API_URL}/vendor/password/reset`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                vendor: data.vendorId,
+                token: data.vendorToken,
+                password: password,
+                confirmPassword: confirmPassword
+            })
+        })
+            .then(r=>r.json())
+            .then((response)=>{
+                if(response.error){
+                    notify("error", response.error.message);
+                }else{
+                    goto("/login");
+                }
+            })
+            .catch((err)=>{
+                notify("error", "Something went wrong, try refreshing the page");
+            })
+            .finally(()=>{
+                loader = false;
+            });
     }
 </script>
 
 <svelte:head>
     <title>Password Reset | Inlet.Shop</title>
 </svelte:head>
+
+{#if loader}
+    <Loader/>
+{/if}
+
+{#if notifier}
+    <Notifier
+        type={notifier.type}
+        message={notifier.message}
+    />
+{/if}
 
 <div class="container">
     <img class="logo" src={logo} alt="Inlet Sites logo">
