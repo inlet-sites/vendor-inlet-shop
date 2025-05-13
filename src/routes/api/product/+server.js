82b5103e6a1dcc5645b806428113bf19806997ba
Lee Morgan <lee@leemorgan.dev>
2025-05-13 15:48:55 -0400

    Create server route for updating products.

--- src/routes/api/product/+server.js
+++ src/routes/api/product/+server.js
@@ -9,3 +9,16 @@ export const POST = async ({request, fetch, cookies})=>{
     });
     return new Response(await response.text());
 }
+
+export const PUT = async ({request, fetch, cookies})=>{
+    const data = await request.json();
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/product/${data.get("vendorId")}`, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${cookies.get("vendorToken")}`
+        },
+        body: data
+    });
+    return new Response(await response.text());
+}
