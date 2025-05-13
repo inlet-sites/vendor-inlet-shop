ffbb8c48ec31a482fb3b30cde85ec23fc1333868
Lee Morgan <lee@leemorgan.dev>
2025-05-13 17:35:39 -0400

    Create server route for updating a variation.

--- src/routes/api/product/variation/+server.js
+++ src/routes/api/product/variation/+server.js
@@ -9,3 +9,21 @@ export const POST = async ({request, fetch, cookies})=>{
     });
     return new Response(await response.text());
 }
+
+export const PUT = async ({request, fetch, cookies})=>{
+    const data = await request.json();
+    const url = `${import.meta.env.VITE_API_URL}/product/${data.productId}/variation/${data.variationId}`;
+    const response = await fetch(url, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${cookies.get("vendorToken")}`
+        },
+        body: JSON.stringify({
+            ...data,
+            productId: undefined,
+            variationId: undefined
+        })
+    });
+    return new Response(await response.text());
+}
