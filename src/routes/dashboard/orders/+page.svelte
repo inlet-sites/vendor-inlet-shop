15134e388251fabee99ecb0377f3610a0cf13387
Lee Morgan <lee@leemorgan.dev>
2025-05-31 15:57:56 -0400

    Make order filters collapsable.

--- src/routes/dashboard/orders/+page.svelte
+++ src/routes/dashboard/orders/+page.svelte
@@ -12,6 +12,7 @@
     let declined = $state(false);
     let confirmed = $state(false);
     let shipped = $state(false);
+    let showFilters = $state(false);
 
     if(orders.error) notify("error", orders.error.message);
 
@@ -34,82 +35,102 @@
 <div class="Orders">
     <h1>Orders</h1>
 
-    <form
-        class="search"
-        action="?/search"
-        method="post"
-        use:enhance={search}
-    >
-        <div class="searchDates">
-            <h4>Dates</h4>
-            <label>From
-                <input
-                    type="date"
-                    name="searchFrom"
-                >
-            </label>
+    {#if showFilters}
+        <form
+            class="search"
+            action="?/search"
+            method="post"
+            use:enhance={search}
+        >
+            <button
+                class="closeFilter"
+                aria-label="Hide filters"
+                type="button"
+                onclick={()=>{showFilters = false}}
+            >
+                <svg width="35px" height="35px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor">
+                    <path d="M6 15L12 9L18 15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+            </button>
 
-            <label>To
-                <input
-                    type="date"
-                    name="searchTo"
-                >
-            </label>
-        </div>
+            <div class="searchDates">
+                <h3>Dates</h3>
+                <label class="dateLabel">From
+                    <input
+                        type="date"
+                        name="searchFrom"
+                    >
+                </label>
 
-        <h4>Status</h4>
-        <div class="searchStatus">
-            <label>Unpaid Order
-                <input
-                    type="checkbox"
-                    name="incomplete"
-                    bind:checked={incomplete}
-                >
-            </label>
+                <label class="dateLabel">To
+                    <input
+                        type="date"
+                        name="searchTo"
+                    >
+                </label>
+            </div>
 
-            <label>Paid
-                <input
-                    type="checkbox"
-                    name="paid"
-                    bind:checked={paid}
-                >
-            </label>
+            <h3>Status</h3>
+            <div class="searchStatus">
+                <label>Unpaid Order
+                    <input
+                        type="checkbox"
+                        name="incomplete"
+                        bind:checked={incomplete}
+                    >
+                </label>
 
-            <label>Payment Failed
-                <input
-                    type="checkbox"
-                    name="paymentFailed"
-                    bind:checked={paymentFailed}
-                >
-            </label>
+                <label>Paid
+                    <input
+                        type="checkbox"
+                        name="paid"
+                        bind:checked={paid}
+                    >
+                </label>
 
-            <label>Declined
-                <input
-                    type="checkbox"
-                    name="declined"
-                    bind:checked={declined}
-                >
-            </label>
+                <label>Payment Failed
+                    <input
+                        type="checkbox"
+                        name="paymentFailed"
+                        bind:checked={paymentFailed}
+                    >
+                </label>
 
-            <label>Vendor Confirmed
-                <input
-                    type="checkbox"
-                    name="confirmed"
-                    bind:checked={confirmed}
-                >
-            </label>
+                <label>Declined
+                    <input
+                        type="checkbox"
+                        name="declined"
+                        bind:checked={declined}
+                    >
+                </label>
 
-            <label>Shipped
-                <input
-                    type="checkbox"
-                    name="shipped"
-                    bind:checked={shipped}
-                >
-            </label>
-        </div>
+                <label>Vendor Confirmed
+                    <input
+                        type="checkbox"
+                        name="confirmed"
+                        bind:checked={confirmed}
+                    >
+                </label>
 
-        <button class="button">Search</button>
-    </form>
+                <label>Shipped
+                    <input
+                        type="checkbox"
+                        name="shipped"
+                        bind:checked={shipped}
+                    >
+                </label>
+            </div>
+
+            <button class="button">Search</button>
+        </form>
+    {:else}
+        <button class="filterButton" aria-label="Show filters" onclick={()=>{showFilters = true}}>
+            <p>Filters</p>
+            <svg width="35px" height="35px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor">
+                <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            </svg>
+        </button>
+    {/if}
     
     {#if orders.length > 0}
         {#each orders as order}
@@ -133,20 +154,61 @@
         position: relative;
     }
 
+    .filterButton{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        background: none;
+        border: 1px solid white;
+        color: white;
+        padding: 5px 35px;
+        cursor: pointer;
+    }
+
+    .closeFilter{
+        position: absolute;
+        top: 0;
+        right: 0;
+        background: none;
+        color: white;
+        border: none;
+        cursor: pointer;
+    }
+
+    .filterButton p{
+        font-size: 25px;
+    }
+
     .search{
         display: flex;
         flex-direction: column;
         border: 1px solid white;
         padding: 15px;
+        position: relative;
     }
 
     .searchDates{
+        display: flex;
+        flex-direction: column;
         margin-bottom: 15px;
+    }
+
+    .searchDates h3{
+        margin-top: 15px;
+    }
+
+    .dateLabel{
+        display: flex;
+        justify-content: space-between;
+        width: 200px;
+        margin-bottom: 5px;
     }
 
     .searchStatus{
         display: flex;
-        align-items: center;
+        flex-direction: column;
+        align-items: flex-start;
     }
 
     .searchStatus label{
